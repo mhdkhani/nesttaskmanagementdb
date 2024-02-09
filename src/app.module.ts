dcde304b0f7481497766d242df5a8fd12faf8726
mhdkhani <khaniii.mahdi@gmail.com>
2024-02-09 00:46:54 -0800

    task CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
